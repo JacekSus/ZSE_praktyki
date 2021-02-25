a0d2ba4f26a2e0f3709e4a4a65e8d505a4f5e3aa
JacekSus <79514927+JacekSus@users.noreply.github.com>
2021-02-25 19:21:15 +0100

    praca domowa

---
+++ src/first_week/basics/zadania/zadania.java
@@ -0,0 +1,32 @@
+package first_week.basics.zadania;
+
+public class zadania {
+    public static void main(String[] args) {
+        //zadanie1
+        int A= 14;
+        double B = 21.37;
+        char C= 'E';
+        String D= "Jest_Dobrze";
+        System.out.println("zadnie1"+"\n"+A+"\n"+B+"\n"+C+"\n"+D+"\n");
+        //zadanie2
+        int a=14;
+        int b=15;
+        int suma= a*a + b*b;
+        System.out.println("zadanie2"+"\n"+suma+"\n");
+        //zadanie3
+        int x=7;
+        double sqrt =Math.sqrt(3);
+        double ptrojkata=((x*x)*sqrt)/4;
+        System.out.println("zadanie3"+"\n"+ptrojkata+"\n");
+        //zadanie4
+        String imie= "Jacek";
+        String nazwisko= "Sus";
+        System.out.println("zadanie4"+"\n"+imie+" "+nazwisko);
+
+
+
+
+
+
+    }
+}
